e2c48bc3cf0a30d92cabbb987ad653377c01b119
Thomas Sampson <drummertom999@gmail.com>
2012-07-25 16:31:34 +0100

    > Removed redundant command from CMakeLists.txt > Windows build no longer tries to run msbuild if cmake script fails to prepare solution file correctly

--- make_windows.bat
+++ make_windows.bat
@@ -1,14 +1,16 @@
-:: Windows batch script
+@echo off
 
 :: Create and move to intermediate build directory
 mkdir build\intermediate
 cd build\intermediate
 
-:: Generate ctu Visual Studio solution
+:: Generate Visual Studio solution
 cmake ../../src
+IF NOT %ERRORLEVEL%==0 GOTO finish
 
 :: Build ctu using msbuild
 msbuild ctu.sln
 
 :: Return to original directory
+:finish
 cd ../../
