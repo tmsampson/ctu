dbe139abe71f203793c7df1aab7129cf54b82bef
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2012-07-29 23:57:12 +0100

    > Removed Dummy functionality and test > Removed PropertiesFile (redundant) > Added first unit test for JSONFile

--- src/ctu/main.cpp
+++ src/ctu/main.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
-#include "Dummy.h"
-#include "PropertiesFile.h"
 
 int main(int argc, char* argv[])
 {
-	printf("10 + 2 = %d\r\n", add<int>(10, 2));
-	PropertiesFile props("C://props.json");
 	return 1;
 }
--- src/ctutest/ctutest.cpp
+++ src/ctutest/ctutest.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "tests/TDummyTest.h"
+#include "tests/TJSONFile.h"
 
 int main(int argc, char* argv[])
 {
