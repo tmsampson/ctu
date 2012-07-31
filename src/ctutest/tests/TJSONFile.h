7110e455d5e790a4cfa277a1e6c36ac483ff4c68
Thomas Sampson <drummertom999@gmail.com>
2012-07-31 15:45:04 +0100

    > Added more JSONFile unit tests

--- src/ctutest/tests/TJSONFile.h
+++ src/ctutest/tests/TJSONFile.h
@@ -8,15 +8,19 @@
 #include "BasicTypes.h"
 
 class TJSONFile : public ::testing::Test { };
-static const char* EMPTY_STRING            = "";
-static const char* BAD_PATH                = "<>|,...";
-static const char* NONE_EXISTENT_FILE      = "ctutest_resources/none-existent-file";
-static const char* SAVE_NEW_FILE           = "ctutest_resources/save-new-file";
-static const char* CREATE_NEW_FILE         = "ctutest_resources/create-new-file";
-static const char* SCOPED_SAVE             = "ctutest_resources/scoped-save";
-static const char* IS_LOADED_TEST1         = "ctutest_resources/is-loaded";
-static const char* EXISTING_FILE           = "ctutest_resources/existing-file";
-static const char* EXISTING_MALFORMED_FILE = "ctutest_resources/existing-malfomed-file";
+static const char*  EMPTY_STRING            = "";
+static const char*  BAD_PATH                = "<>|,...";
+static const char*  NONE_EXISTENT_FILE      = "ctutest_resources/none-existent-file";
+static const char*  SAVE_NEW_FILE           = "ctutest_resources/save-new-file";
+static const char*  CREATE_NEW_FILE         = "ctutest_resources/create-new-file";
+static const char*  SCOPED_SAVE             = "ctutest_resources/scoped-save";
+static const char*  IS_LOADED_TEST1         = "ctutest_resources/is-loaded";
+static const char*  EXISTING_FILE           = "ctutest_resources/existing-file";
+static const char*  EXISTING_MALFORMED_FILE = "ctutest_resources/existing-malfomed-file";
+static const float  PI                      = 3.14159f;
+static const double PI_ACCURATE             = 3.1415926535897932384626433832795028841;
+static const double MAX_DOUBLE              = std::numeric_limits<double>::max();
+
 
 // ************************************************
 // JSONFile Constructor Tests
@@ -65,7 +69,7 @@ TEST_F(TJSONFile, Destructor_OutOfScopeWihtoutSaving_FileExists)
 // ************************************************
 // JSONFile::IsLoaded Tests
 // ************************************************
-TEST_F(TJSONFile, IsLoaded_CreateNewFile_IsLoadedReturnsFalseUntilSave)
+TEST_F(TJSONFile, IsLoaded_CreateNewFile_ReturnFalseUntilSave)
 {
 	JSONFile jfile(IS_LOADED_TEST1);
 	ASSERT_FALSE(jfile.IsLoaded());
@@ -73,13 +77,13 @@ TEST_F(TJSONFile, IsLoaded_CreateNewFile_IsLoadedReturnsFalseUntilSave)
 	ASSERT_TRUE(jfile.IsLoaded());
 }
 
-TEST_F(TJSONFile, IsLoaded_LoadExistingFile_IsLoadedReturnsTrue)
+TEST_F(TJSONFile, IsLoaded_LoadExistingFile_ReturnTrue)
 {
 	JSONFile jfile(EXISTING_FILE);
 	ASSERT_TRUE(jfile.IsLoaded());
 }
 
-TEST_F(TJSONFile, IsLoaded_LoadExistingMalformedFile_IsLoadedReturnsFalse)
+TEST_F(TJSONFile, IsLoaded_LoadExistingMalformedFile_ReturnFalse)
 {
 	JSONFile jfile(EXISTING_MALFORMED_FILE);
 	ASSERT_FALSE(jfile.IsLoaded());
@@ -106,36 +110,97 @@ TEST_F(TJSONFile, Set_SetIntMinus2_GetIntReturnsMinus2)
 {
 	JSONFile jfile(EMPTY_STRING);
 	jfile.Set<s32>("myUInt", -2);
-	ASSERT_EQ(jfile.Get<s32>("myUInt"), -2);
+	ASSERT_EQ(-2, jfile.Get<s32>("myUInt"));
 }
 
 TEST_F(TJSONFile, Set_SetUInt41_GetUIntReturns41)
 {
 	JSONFile jfile(EMPTY_STRING);
 	jfile.Set<u32>("myInt", 41);
-	ASSERT_EQ(jfile.Get<u32>("myInt"), 41);
+	ASSERT_EQ(41, jfile.Get<u32>("myInt"));
 }
 
 TEST_F(TJSONFile, Set_SetFloatPI_GetFloatReturnsPI)
 {
-	static const float fPI = 3.14159f;
 	JSONFile jfile(EMPTY_STRING);
-	jfile.Set<float>("myFloat", fPI);
-	ASSERT_EQ(jfile.Get<float>("myFloat"), fPI);
+	jfile.Set<float>("myFloat", PI);
+	ASSERT_EQ(PI, jfile.Get<float>("myFloat"));
 }
 
 TEST_F(TJSONFile, Set_SetDoubleMax_GetDoubleReturnsMax)
 {
-	static const double max = std::numeric_limits<double>::max();
 	JSONFile jfile(EMPTY_STRING);
-	jfile.Set<double>("myDouble", max);
-	ASSERT_EQ(jfile.Get<double>("myDouble"), max);
+	jfile.Set<double>("myDouble", MAX_DOUBLE);
+	ASSERT_EQ(MAX_DOUBLE, jfile.Get<double>("myDouble"));
 }
 
 TEST_F(TJSONFile, Set_SetStringFoo_GetStringReturnsFoo)
 {
 	JSONFile jfile(EMPTY_STRING);
 	jfile.Set<std::string>("myString", "Foo");
-	ASSERT_EQ(jfile.Get<std::string>("myString"), "Foo");
+	ASSERT_EQ("Foo", jfile.Get<std::string>("myString"));
+}
+
+TEST_F(TJSONFile, Set_SuplyBlankKey_ReturnFalse)
+{
+	JSONFile jfile(EMPTY_STRING);
+	ASSERT_FALSE(jfile.Set<u32>("", 101));
+}
+
+TEST_F(TJSONFile, Set_OverWriteExistingValue_GetReturnsNewValue)
+{
+	JSONFile jfile(EMPTY_STRING);
+	jfile.Set<std::string>("myString", "Foo");
+	ASSERT_EQ("Foo", jfile.Get<std::string>("myString"));
+	jfile.Set<std::string>("myString", "Bar");
+	ASSERT_EQ("Bar", jfile.Get<std::string>("myString"));
+}
+
+// ************************************************
+// JSONFile::ContainsKey<T> Tests
+// ************************************************
+TEST_F(TJSONFile, ContainsKey_TestInvalidKey_ReturnFalse)
+{
+	JSONFile jfile(EMPTY_STRING);
+	ASSERT_FALSE(jfile.ContainsKey("Foo"));
+}
+
+TEST_F(TJSONFile, ContainsKey_TestExistingKey_ReturnTrue)
+{
+	JSONFile jfile(EMPTY_STRING);
+	jfile.Set<u32>("WeeksInYear", 52);
+	ASSERT_TRUE(jfile.ContainsKey("WeeksInYear"));
+}
+
+TEST_F(TJSONFile, ContainsKey_TestEmptyKey_ReturnFalse)
+{
+	JSONFile jfile(EMPTY_STRING);
+	ASSERT_FALSE(jfile.ContainsKey(""));
+}
+
+// ************************************************
+// JSONFile::Get<T> Tests
+// ************************************************
+TEST_F(TJSONFile, Get_GetFromExistingFile_AllValuesCorrect)
+{
+	JSONFile jfile(EXISTING_FILE);
+	ASSERT_EQ(true, jfile.Get<bool>("myBool"));
+	ASSERT_EQ(-2, jfile.Get<s32>("myInt"));
+	ASSERT_EQ(41, jfile.Get<u32>("myUInt"));
+	ASSERT_EQ(PI, jfile.Get<float>("myFloat"));
+	ASSERT_EQ(PI_ACCURATE, jfile.Get<double>("myDouble"));
+	ASSERT_EQ("Foo", jfile.Get<std::string>("myString"));
+}
+
+TEST_F(TJSONFile, Get_SupplyDefaultUIntVal_ReturnDefaultUIntVal)
+{
+	JSONFile jfile(EMPTY_STRING);
+	ASSERT_EQ(52, jfile.Get<u32>("WeeksInYear", 52));
+}
+
+TEST_F(TJSONFile, Get_SupplyDefaultStringVal_ReturnDefaultStringVal)
+{
+	JSONFile jfile(EMPTY_STRING);
+	ASSERT_EQ("Norris", jfile.Get<std::string>("Chuck", "Norris"));
 }
 #endif
