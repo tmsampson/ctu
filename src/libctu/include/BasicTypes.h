fedaf2e7ecf254cf799afa3036fbdfb38d75f8cc
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2012-07-31 12:32:54 +0100

    > Added BasicTypes.h to include some abbreviated typedefs

---
+++ src/libctu/include/BasicTypes.h
@@ -0,0 +1,11 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef char s8;
+typedef unsigned char u8;
+typedef int s32;
+typedef unsigned int u32;
+typedef long long s64;
+typedef unsigned long long u64;
+
+#endif
