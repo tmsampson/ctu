9df6b7c4d2f98a3644508ba82338a6af02e6ad33
Thomas Sampson <drummertom999@gmail.com>
2012-07-31 14:59:20 +0100

    > Updated linux build script to copy over ctutest resources

--- make_linux.sh
+++ make_linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+CTU_ROOT_DIR=$PWD
+CTU_BUILD_DIR=$CTU_ROOT_DIR/build
+CTU_INTERMEDIATE_DIR=$CTU_BUILD_DIR/intermediate
+CTU_TEST_RESOURCES_DIR=$CTU_ROOT_DIR/src/ctutest/ctutest_resources
 
 function exit_if_failed
 {
@@ -10,11 +14,11 @@ function exit_if_failed
 }
 
 # Create and move to intermediate build directory
-mkdir -p build/intermediate
-cd build/intermediate
+mkdir -p $CTU_INTERMEDIATE_DIR
+cd $CTU_INTERMEDIATE_DIR
 
 # Generate ctu g++ makefile
-cmake ../../src
+cmake $CTU_ROOT_DIR/src
 exit_if_failed
 
 # Build ctu using g++
@@ -26,10 +30,11 @@ clear
 echo =====================================================
 echo = R U N N I N G = U N I T = T E S T S
 echo =====================================================
-cd ../
-mkdir unit_test_resources
+cd $CTU_BUILD_DIR
+mkdir $CTU_BUILD_DIR/ctutest_resources
+cp -r $CTU_TEST_RESOURCES_DIR/* $CTU_BUILD_DIR/ctutest_resources
 ./ctutest
-rm -r unit_test_resources
+rm -r $CTU_BUILD_DIR/ctutest_resources
 
 # Return to original directory
-cd ../
+cd $CTU_ROOT_DIR
