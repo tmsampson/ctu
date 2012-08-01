60e79e86ab77b921dd65100307b94971e678320a
Thomas Sampson <drummertom999@gmail.com>
2012-08-02 00:17:22 +0100

    > JSONFile no longer trashes non-JSON files on save/destructor call > Added tests to prove the above

--- src/libctu/include/JSONFile.h
+++ src/libctu/include/JSONFile.h
@@ -20,7 +20,6 @@ class JSONFile
 		T Get(const std::string& key, const T& defaultValue);
 		template <typename T>
 		T Get(const std::string& key);
-
 		template <typename T>
 		bool Set(const std::string& key, const T& value, bool saveImmediate = false);
 
@@ -31,7 +30,7 @@ class JSONFile
 		std::string m_path;
 		Json::Value m_root;
 		Json::Reader m_reader;
-		bool m_isLoaded;
+		bool m_bLoadFailed, m_bParseFailed;
 };
 
 #include "JSONFile.inl"
