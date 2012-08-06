8901fbc80c8068a3a87df337cfcc5b76f1885686
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2012-08-06 14:37:38 +0100

    > Added new test 'TouchFile_PassBadFilename_ReturnFalse'

--- src/ctu/main.cpp
+++ src/ctu/main.cpp
@@ -1,8 +1,55 @@
 #include <stdio.h>
 #include "Utils.h"
+#include "JSONFile.h"
+
+namespace ctu
+{
+	const std::string JK_CURRENT_TASK_LIST = "currentTaskList";
+	const std::string JK_VERBOSE = "verbose";
+
+	bool RunStartupChecks(JSONFile& configFile)
+	{
+		// Get path to current task list
+		std::string taskListPath = configFile.Get<std::string>(ctu::JK_CURRENT_TASK_LIST);
+
+		// Is the current task list set?
+		if(taskListPath.empty())
+		{
+			printf("WARNING: No task list is currently set.\r\n");
+
+			do
+			{
+				printf("Where would you like your task list to be created? (leave blank for default)\r\n");
+				printf("Task List Path: ");
+				std::getline(std::cin, taskListPath);
+
+				if(!taskListPath.empty())
+				{
+					if(!Utils::DirectoryExists(taskListPath))
+						printf("ERROR: The directory specified does not exist, please try again\r\n");
+					continue;
+				}
+
+				taskListPath = Utils::GetDefaultTaskListDirectory();
+			}
+			while(!Utils::DirectoryExists(taskListPath));
+
+			taskListPath = taskListPath + Utils::PATH_SEPARATOR + "tasks.txt";
+			configFile.Set<std::string>(ctu::JK_CURRENT_TASK_LIST, taskListPath);
+			printf("Creating %s    ", taskListPath.c_str());
+		}
+
+		// Ensure values are set OR default
+		configFile.Set<bool>(JK_VERBOSE, configFile.Get<bool>(JK_VERBOSE, false));
+
+		configFile.Save();
+		return true;
+	}
+}
 
 int main(int argc, char* argv[])
 {
-	printf("result = %s\r\n", Utils::GetExecutableDir().c_str());
-	return 1;
+	JSONFile configFile(Utils::GetConfigFilePath());
+	ctu::RunStartupChecks(configFile);
+	return 0;
 }
