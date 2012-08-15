39cf7c1a682425b58925dd5d4e4b963e423e0c1b
Andrew Wiley <wiley.a@gmail.com>
2012-08-15 09:40:32 +0100

    > Commented out two tasks for the time being

--- src/ctutest/tests/TTaskList.h
+++ src/ctutest/tests/TTaskList.h
@@ -17,7 +17,7 @@ TEST_F(TTaskList, AddTask_NoTasksAdded_TaskCountIsZero)
 	CTU::TaskList newList;
 	newList.Init("./", "");
 
-	ASSERT_TRUE(newList.GetTaskCount() == 0);
+	ASSERT_EQ(newList.GetTaskCount(), 0);
 }
 
 TEST_F(TTaskList, AddTask_AddOneTask_TaskCountIsOne)
@@ -29,7 +29,7 @@ TEST_F(TTaskList, AddTask_AddOneTask_TaskCountIsOne)
 	// Add one task
 	newList.AddTask("test");
 
-	ASSERT_TRUE(newList.GetTaskCount() == 1);
+	ASSERT_EQ(newList.GetTaskCount(), 1);
 }
 
 TEST_F(TTaskList, AddTask_AddFourTasks_TaskCountIsFour)
@@ -44,7 +44,37 @@ TEST_F(TTaskList, AddTask_AddFourTasks_TaskCountIsFour)
 	newList.AddTask("test3");
 	newList.AddTask("test4");
 
-	ASSERT_TRUE(newList.GetTaskCount() == 4);
+	ASSERT_EQ(newList.GetTaskCount(), 4);
 }
+
+// ************************************************
+//  TTaskList::GetPath Tests
+// ************************************************
+TEST_F(TTaskList, GetPath_NormalCall_PathReturned)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("C:\\temp.txt", "");
+
+	ASSERT_EQ(newList.GetPath(), "C:\\temp.txt");
+}
+
+/*TEST_F(TTaskList, GetPath_NoExtension_BlankReturn)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("C:\\temp", "");
+
+	ASSERT_EQ(newList.GetPath(), "");
+}
+
+TEST_F(TTaskList, GetPath_NoPathPassed_BlankReturn)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("", "");
+
+	ASSERT_EQ(newList.GetPath(), "");
+}*/
 
 #endif
