a1ca42a4c2dc5346a662de66cd07fd30e195fc4e
Thomas Sampson <drummertom999@gmail.com>
2012-08-17 11:27:54 +0100

    > Cleaned up Utils::Assert (now uses compiler intrinsics/built-ins) > Rename CPU_ASSERT -> CTU_ASSERT

--- src/libctu/include/Utils.h
+++ src/libctu/include/Utils.h
@@ -2,11 +2,12 @@
 #define UTILS_H_
 
 #include <string>
-#include <memory>
 #include "BasicTypes.h"
-#if !defined(_WIN32)
+
+#if defined(_WIN32)
+	#include <memory>
+#else
 	#include <tr1/memory>
-	#include <assert.h>
 #endif
 
 namespace Utils
@@ -48,38 +49,8 @@ namespace Utils
 	extern std::string StringTrim(const std::string& str);
 	extern std::string StringTrim(const std::string& str, const std::string& trimChars);
 	extern std::string StringToLower(const std::string& str);
-
-	inline void Assert(bool bCondition, const std::string& file, const std::string& function, u32 uLine, const std::string& message)
-	{
-		if(bCondition)
-			return;
-
-		Utils::PrintLine(Utils::EColour::RED, "***************************************************");
-		Utils::PrintLine(Utils::EColour::RED, " A S S E R T   F A I L E D");
-		Utils::PrintLine(Utils::EColour::RED, "***************************************************");
-		if(message.size())
-			Utils::PrintLine(Utils::EColour::RED, "  Message: %s", message.c_str());
-		Utils::PrintLine(Utils::EColour::RED, "     File: %s", file.c_str());
-		Utils::PrintLine(Utils::EColour::RED, " Function: %s", function.c_str());
-		Utils::PrintLine(Utils::EColour::RED, "     Line: %d", uLine);
-		Utils::PrintLine(Utils::EColour::RED, "***************************************************");
-		#if defined(_WIN32)
-			__asm
-			{
-				int 4;
-			}
-		#else
-			assert(false);
-		#endif
-	}
-
-	#if defined(NDEBUG)
-		#define CPU_ASSERT(expression, message) ((void)0)
-		#define CPU_ASSERT_ONLY(expression) ((void)0)
-	#else
-		#define CPU_ASSERT(expression, message) Utils::Assert((expression), __FILE__, __FUNCTION__, __LINE__, (message))
-		#define CPU_ASSERT_ONLY(expression) Utils::Assert((expression), __FILE__, __FUNCTION__, __LINE__, "")
-	#endif
 }
+
+#include "CTUAssert.inl"
 
 #endif
