f512e7d7c73ecc6f248dfbd05f31a866bd911267
Andrew Wiley <wiley.a@gmail.com>
2012-08-18 21:39:56 +0100

    > Added CtU::TaskList::RemoveTask unit tests

--- src/ctutest/tests/TTaskList.h
+++ src/ctutest/tests/TTaskList.h
@@ -48,6 +48,86 @@ TEST_F(TTaskList, AddTask_AddFourTasks_TaskCountIsFour)
 }
 
 // ************************************************
+//  TTaskList::RemoveTask Tests
+// ************************************************
+TEST_F(TTaskList, RemoveTask_RemoveOneTask_TaskCountIsOne)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("./", "");
+
+	// Add two tasks
+	newList.AddTask("test1");
+	newList.AddTask("test2");
+
+	// Remove one task
+	newList.RemoveTask(1);
+
+	ASSERT_EQ(1, newList.GetTaskCount());
+}
+
+TEST_F(TTaskList, RemoveTask_RemoveFourTasks_TaskCountIsFour)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("./", "");
+
+	// Add eight tasks
+	newList.AddTask("test1");
+	newList.AddTask("test2");
+	newList.AddTask("test3");
+	newList.AddTask("test4");
+	newList.AddTask("test5");
+	newList.AddTask("test6");
+	newList.AddTask("test7");
+	newList.AddTask("test8");
+
+	// Remove four tasks
+	newList.RemoveTask(1);
+	newList.RemoveTask(2);
+	newList.RemoveTask(3);
+	newList.RemoveTask(4);
+
+	ASSERT_EQ(4, newList.GetTaskCount());
+}
+
+TEST_F(TTaskList, RemoveTask_RemoveTaskFirstIndex_CorrectTaskIsReturned)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("./", "");
+
+	// Add three tasks
+	newList.AddTask("test1");
+	newList.AddTask("test2");
+	newList.AddTask("test3");
+
+	// Remove first task
+	std::string taskStr = "";
+	newList.RemoveTask(1, taskStr);
+
+	ASSERT_EQ("test1", taskStr);
+}
+
+TEST_F(TTaskList, RemoveTask_RemoveTaskLastIndex_CorrectTaskIsReturned)
+{
+	// Create a new list and initialise it
+	CTU::TaskList newList;
+	newList.Init("./", "");
+
+	// Add three tasks
+	newList.AddTask("test1");
+	newList.AddTask("test2");
+	newList.AddTask("test3");
+
+	// Remove last task
+	std::string taskStr = "";
+	newList.RemoveTask(newList.GetTaskCount(), taskStr);
+
+	ASSERT_EQ("test3", taskStr);
+}
+
+// ************************************************
 //  TTaskList::GetPath Tests
 // ************************************************
 TEST_F(TTaskList, GetPath_NormalCall_PathReturned)
