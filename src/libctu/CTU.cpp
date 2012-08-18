5823861e6f59350072eb03be3064f020c00674e0
Andrew Wiley <wiley.a@gmail.com>
2012-08-18 22:55:52 +0100

    > Added CmdCount which outputs the number of tasks in the active task list

--- src/libctu/CTU.cpp
+++ src/libctu/CTU.cpp
@@ -9,6 +9,7 @@
 // CTU Commands
 #include "commands/CmdAdd.h"    // add
 #include "commands/CmdClear.h"  // clear
+#include "commands/CmdCount.h"  // count
 #include "commands/CmdHelp.h"   // help
 #include "commands/CmdList.h"   // list
 #include "commands/CmdRemove.h" // remove
@@ -100,6 +101,7 @@ int CTU::Begin(const std::vector<std::string>& args)
 	// Register all commands
 	commandMgr.RegisterCommand<CTU::Commands::CmdAdd>();    // add
 	commandMgr.RegisterCommand<CTU::Commands::CmdClear>();  // clear
+	commandMgr.RegisterCommand<CTU::Commands::CmdCount>();  // count
 	commandMgr.RegisterCommand<CTU::Commands::CmdHelp>();   // help
 	commandMgr.RegisterCommand<CTU::Commands::CmdList>();   // list
 	commandMgr.RegisterCommand<CTU::Commands::CmdRemove>(); // remove
