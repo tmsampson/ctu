db52c6ad477d03b23fd306758e1ce3adb3cf1e91
Thomas Sampson <32266729+tmsampson@users.noreply.github.com>
2012-08-19 09:10:31 +0100

    > Added "config" command (CTU::Commands::CmdConfig) to open the ctu.settings file in an external editor

---
+++ src/libctu/commands/CmdConfig.h
@@ -0,0 +1,34 @@
+#include "CTU.h"
+#include "Utils.h"
+#include "CommandMgr.h"
+extern ConfigFile* pConfigFile;
+
+namespace CTU
+{
+	namespace Commands
+	{
+		class CmdConfig : public CTU::Command
+		{
+			public:
+				virtual std::string GetName() const    { return "config"; }
+				virtual std::string GetSummary() const { return "opens ctu.settings file for editing"; }
+				virtual std::string GetUsage() const
+				{
+					return std::string("ctu config\r\n\r\n") +
+					       std::string("opens ctu.settings file for editing\r\n");
+				}
+
+				virtual bool Validate(const CTU::Command::ArgList& args) const
+				{
+					return args.size() == 0;
+				}
+
+				virtual bool Execute(const CTU::Command::ArgList& args, CTU::TaskList& taskList)
+				{
+					std::string command = pConfigFile->Get<std::string>("editor") + " \"" + pConfigFile->GetPath() + "\"";
+					system(command.c_str());
+					return true;
+				}
+		};
+	}
+}
