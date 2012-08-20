af222be9ebbfa84acfd982adb4d1df644c115f98
Thomas Sampson <drummertom999@gmail.com>
2012-08-20 12:14:27 +0100

    > Added packaging routine for OSX (zip package), simply run './make_osx package'

--- make_osx.sh
+++ make_osx.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+CTU_ROOT_DIR=$PWD
+CTU_BUILD_DIR=$CTU_ROOT_DIR/build
+
+function exit_if_failed
+{
+	# Exit early if previous command failed
+	if ! (( $? == 0 )); then
+	cd ../../ # Return to original directory
+	exit
+	fi
+}
 
 # For now the linux build works just fine
 sh make_linux.sh
+exit_if_failed
+
+# Build package?
+if [ "$1" == package ]; then
+	printf "\r\n"
+	echo =====================================================
+	echo = G E N E R A T I N G = Z I P = P A C K A G E
+	echo =====================================================
+	cd $CTU_BUILD_DIR/intermediate
+	cpack -G ZIP
+fi
